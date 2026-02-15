2f932a5085451d50b9de7bfaf1fa31cef6138f93
Aditya Prasad <aditya.prasad@devslane.com>
2026-02-15 12:48:07 +0530

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import { WebRoutes } from "./routes/WebRoutes";
 import Auth from "./pages/Auth/Auth";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
+    <>
+    <Navbar/>
     <Routes>
       <Route path={WebRoutes.HOME()} element={<Home/>} />
       <Route path={WebRoutes.AUTH()} element={<Auth/>} />
     </Routes>
+    </>
   )
 }
 
